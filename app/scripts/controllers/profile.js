27996282c3c29cb136923ec9f93446beae72b664
Alberto Villalobos <alberto@albertovillalobos.me>
2014-06-26 19:23:56 -0700

    added tons of stuff

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,8 +1,62 @@
+/* ==========================================================================
+
+
+	Main page after logging in
+
+	- Only show comfirmed or pending hangouts!
+   
+========================================================================== */
+
+
+
+
 'use strict';
 
 angular.module('personalsiteAngularApp')
   .controller('ProfileCtrl', function ($scope, $http) {
-    // $http.get('/api/awesomeThings').success(function(awesomeThings) {
-    //   $scope.awesomeThings = awesomeThings;
-    // });
+
+
+
+  	// Array of events
+  	$scope.events = [
+  		// {
+  		// 	name:'',
+  		// 	starttime: '7:00',
+  		// 	endtime: '7:30',
+  		// 	status: 'empty'
+  		// },
+  		// {
+  		// 	name: 'work',
+  		// 	starttime: '7:30',
+  		// 	endtime: '9:30',
+  		// 	status: 'busy'
+  		// },
+  		{
+  			name:'Ask to Hang with Liam',
+  			starttime: '9:30',
+  			endtime: '10:00',
+  			status: 'potential'  			
+  		},
+  		// {
+  		// 	name: '',
+  		// 	starttime: '10:00',
+  		// 	endtime: '10:30',
+  		// 	status: 'empty'
+  		// },
+  		{
+  			name: 'You are hanging out with Liam',
+  			starttime: '10:30',
+  			endtime: '11:30',
+  			status: 'comfirmed'
+  		},
+  		// {
+  		// 	name: '',
+  		// 	starttime: '11:30',
+  		// 	endtime:'24:00',
+  		// 	status: 'empty'
+  		// }
+  	];
+
+
+  	console.log($scope.events);
   });
